12bb2faea17ffaea067650226afc6a085b73a852
George Bush Odhiambo <30529428+Bushisky@users.noreply.github.com>
2025-08-14 13:10:22 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,3 @@
-<?php
-$host = "localhost";
-$user = "kip";
-$pass = "SPICEGlobal@1234";
-$dbname = "powerbi";
-
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    error_log("Database connection failed: " . $conn->connect_error);
-    exit();
-}
-
-// No closing PHP tag, no echo, no space, no die()
 
 
 
